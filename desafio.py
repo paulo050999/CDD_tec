6db3cd09af8bfd8cfc8b104baaa2d299b3c9e87d
paulo050999 <paulodouglas2@gmail.com>
2024-09-05 16:12:01 -0300

    correção

--- desafio.py
+++ desafio.py
@@ -4,41 +4,36 @@
 # entrada2: 14:20
 
 # saída 6:05
-h1 = int(input("Digite a hora 1: "))
-m1 = int(input("Digite o minuto 1: "))
+h1 = int(input("Digite a hora 1: \n"))
+m1 = int(input("Digite o minuto 1: \n"))
 
-if 0 <= h1 <= 24 and 0 <= m1 <60:
+if (0 <= h1 and h1 <= 24 and 0 <= m1 and m1 <=60):
+    h2 = int(input("Digite a hora 2: \n"))
+    m2 = int(input("Digite o minuto 2: \n"))
+    if 0 <= h2 <= 24 and 0 <= m2 < 60:
+        if h2 > 12:
+            h2 -= 12
+        if h1 > 12:
+            h1 -= 12
 
-  h2 = int(input("Digite a hora 2: "))
-  m2 = int(input("Digite o minuto 1: "))
+        somamin = m1 + m2
+        somahor = (h1 + h2)
 
-  if 0 <= h2 <= 24 and 0 <= m2 < 60:
+        if somamin >= 60:
+            somamin -= 60
+            somahor +=1
 
-    if h2 > 12:
-        h2 -= 12
-    if h1 > 12:
-        h1 -= 12
+        if somahor > 12:
+            somahor +=12
 
-    somamin = m1 + m2
-    somahor = (h1 + h2)
-
-    if somamin >= 60:
-        somamin -= 60
-        somahor +=1
-
-    if somahor > 12:
-        somahor +=12
-
-    if somahor > 24:
-        somahor -=24
-
-    print (f"A saída é {somahor:02d}: {somamin:02d}")
-
+        if somahor > 24:
+            somahor -=12
+        print("--------------A SAIDA É------------------")
+        print (f"-------------- {somahor:02d}: {somamin:02d} ----")
+    else:
+         print("Hora ou minutos da segunda entrada inválidos")
 else:
-     print("Hora ou minutos da segunda entrada inválidos")
-else:
-print("Hora ou minutos da primeira entrada inválidos")
-
+    print("Hora ou minutos da primeira entrada inválidos")
 
 
 # if 0 <= hora1 <= 24 and 0 <= minuto1 <60;
