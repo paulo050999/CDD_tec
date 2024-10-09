5d1c54ec477e5b66e17347a384e5d4da7ad3554f
paulo050999 <paulodouglas2@gmail.com>
2024-10-09 16:43:06 -0300

    09/10.2

---
+++ avançado/aulas.po.o/P.O.O.py
@@ -0,0 +1,20 @@
+"""
+
+
+
+Classe = molde, de uma classe saem objetos;
+Objetos = instanciar é criar um objeto a partir de uma classe;
+Métodos = toda classe tem dois compoonentes, os métodos e os atributos; Características do objeto são atributos; Méto-
+dos são ações.
+
+4 pilares da p.o.o:
+
+1° abstração - Não ter o conhecimentos dos detalhes de funcionamento para usar.
+2° encapsulamento - Significa proteger.
+3° Herança - Uma classe transmite a outra classe métodos e atributos, a classe mãe/superclasse transmite pa-
+ra a classe filha/subclasse.
+4° polimorfismo - alterar características herdades para utilizá-las em outras funções
+
+
+
+"""
---
+++ avançado/aulas.po.o/main.py
@@ -0,0 +1,12 @@
+from biblioteca import *
+
+p1 = Pessoa("João", 75, 21)
+p2 = Pessoa("Luiza", 60, 19)
+print(p1.nome, p1.idade, p1.peso)
+
+print(p2)
+
+
+
+p2.comer()
+p2.pararDeComer()
