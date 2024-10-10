8e02a49cd83612bc1a94e0a05894247579264a68
paulo050999 <paulodouglas2@gmail.com>
2024-10-10 20:38:07 -0300

    merge

--- biblioteca.py
+++ biblioteca.py
@@ -10,7 +10,7 @@ def imprime_nome(nome):
 #from biblioteca import par
 #par(6)
 
-def iogurte(numero):
+def piramide(numero):
     for i in range(1, numero + 1):
         for x in range(i):
             print(i, end=" ")
@@ -68,7 +68,13 @@ def lista_Reversa(c):
 def primos(k):
     list=[]
 
-
+def semrepeticao(l):
+    l = [10,12,12,31,4,4,5,31,6,7,6,8]
+    nlist = []
+    for x in l:
+        if x not in nlist:
+            nlist.append(x)
+    print(nlist)
 
 
 
